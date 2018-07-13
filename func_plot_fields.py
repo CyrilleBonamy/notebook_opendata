a38b3eabd2761dfe01024a923f8929aa86d354f4
Antoine.Mathieu <mathieu7an@meige4pcjc69.legi.grenoble-inp.fr>
2018-07-13 11:44:28 +0200

    modif func_plot_fields

--- func_plot_fields.py
+++ func_plot_fields.py
@@ -1,10 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import requests
 from lxml import etree
-import numpy as np
 import ipywidgets as widgets
-from IPython.display import display, clear_output
-import matplotlib.pyplot as plt
 
 
 def write_in_widget():
@@ -12,9 +11,8 @@ def write_in_widget():
     global path
     global rootgrp
     global oldvalue
-    
+
     value = select.value
-    
     if value == '.':
         path = base_url
     elif value == '..':
@@ -33,14 +31,14 @@ def write_in_widget():
         path = base_url
         catalog_file = requests.get(path + 'catalog.xml')
         tree = etree.fromstring(catalog_file.content)
-        
+
     options = ['', '.', '..']
-    
+
     if not value.endswith('.nc'):
         for children in tree[2]:
             if children.get('ID').endswith('/') or children.get('ID').endswith('.nc'):
                 options.append(children.get('name'))
-        select.options = options 
+        select.options = options
     else:
         if value in loadedfiles[0]:
             rootgrp = loadedfiles[1][loadedfiles[0].index(value)]
@@ -56,7 +54,6 @@ def write_in_widget():
         y_data.options = datasets_name
         u_data.options = datasets_name
         v_data.options = datasets_name
-    
 
 
 def f(select_loc, x_data_loc, y_data_loc, u_data_loc, v_data_loc):
@@ -75,7 +72,7 @@ def h():
                      vector=widgets.Checkbox(value=False,
                                              description='Display vectors',
                                              disabled=False
-                                            ),
+                                             ),
                      )
 
 
@@ -86,24 +83,24 @@ def g(variable, vector):
         except:
             print('Invalid netCDF file or invalid dataset selection')
 
-            
+
 def plot_func(variable, vector, x_data, y_data, u_data, v_data):
-    # Figure size
     fig_xsize = 15
     fig_ysize = 10
     font_size = 25
-    fig = plt.figure(num=0, figsize=(fig_xsize, fig_ysize), dpi=60,
-                     facecolor='w', edgecolor='w')
+    plt.figure(num=0, figsize=(fig_xsize, fig_ysize), dpi=60,
+               facecolor='w', edgecolor='w')
     plt.rcParams.update({'font.size': font_size})
     plt.gca().set_aspect('equal')
-    #axis names
     xaxis_name = 'i'
     yaxis_name = 'j'
     plt.xlabel(xaxis_name)
     plt.ylabel(yaxis_name)
     contourcolor = plt.cm.coolwarm
-    xlin = np.linspace(0, len(rootgrp.variables[x_data.value])-1, len(rootgrp.variables[x_data.value]))
-    ylin = np.linspace(0, len(rootgrp.variables[y_data.value])-1, len(rootgrp.variables[y_data.value]))
+    xlin = np.linspace(0, len(rootgrp.variables[x_data.value])-1,
+                       len(rootgrp.variables[x_data.value]))
+    ylin = np.linspace(0, len(rootgrp.variables[y_data.value])-1,
+                       len(rootgrp.variables[y_data.value]))
     x, y = np.meshgrid(xlin, ylin)
     try:
         if variable == 'norm':
@@ -139,18 +136,15 @@ def plot_func(variable, vector, x_data, y_data, u_data, v_data):
             varplot = rootgrp.variables[u_data.value][:, :]
             mycontour = plt.contourf(varplot, 50, cmap=contourcolor)
             cbar = plt.colorbar(mycontour)
-            #cbar.ax.set_ylabel(rootgrp.variables[variable].name)
-            #                   " (" + rootgrp.variables[u_data.value].units + ")")
         else:
             varplot = rootgrp.variables[v_data.value][:, :]
             mycontour = plt.contourf(varplot, 50, cmap=contourcolor)
             cbar = plt.colorbar(mycontour)
-            #cbar.ax.set_ylabel(rootgrp.variables[variable].name)
-            #                   " (" + rootgrp.variables[v_data.value].units + ")")            
         if vector:
             plt.quiver(x, y, rootgrp.variables[u_data.value][:, :],
-                       variables[v_data.value][0, 0, :, :])
-           
+                       rootgrp.variables[v_data.value][0, 0, :, :])
+
+
 def plot_field():
     global base_url
     global path
@@ -159,12 +153,12 @@ def plot_field():
     global y_data
     global u_data
     global v_data
+    global loadedfiles
 
     base_url = 'http://servdap.legi.grenoble-inp.fr:80/opendap/'
     path = base_url
-    
-    global loadedfiles
-    loadedfiles = [[],[]]
+    loadedfiles = [[], []]
+
     select = widgets.Select(
                             options=['.', '..'],
                             value='.',
@@ -172,30 +166,30 @@ def plot_field():
                             disabled=False
                             )
     x_data = widgets.Dropdown(
-                             options=['choose a dataset'],
-                             value='choose a dataset',
-                             description='x',
-                             disabled=False
+                              options=['choose a dataset'],
+                              value='choose a dataset',
+                              description='x',
+                              disabled=False
                              )
     y_data = widgets.Dropdown(
-                             options=['choose a dataset'],
-                             value='choose a dataset',
-                             description='y',
-                             disabled=False
+                              options=['choose a dataset'],
+                              value='choose a dataset',
+                              description='y',
+                              disabled=False
                              )
-        
+  
     u_data = widgets.Dropdown(
-                             options=['choose a dataset'],
-                             value='choose a dataset',
-                             description='u',
-                             disabled=False
+                              options=['choose a dataset'],
+                              value='choose a dataset',
+                              description='u',
+                              disabled=False
                              )
-    
+
     v_data = widgets.Dropdown(
-                             options=['choose a dataset'],
-                             value='choose a dataset',
-                             description='v',
-                             disabled=False
+                              options=['choose a dataset'],
+                              value='choose a dataset',
+                              description='v',
+                              disabled=False
                              )
-    widgets.interact(f, select_loc=select, x_data_loc=x_data, y_data_loc=y_data,
-                     u_data_loc=u_data, v_data_loc=v_data)
+    widgets.interact(f, select_loc=select, x_data_loc=x_data,
+                     y_data_loc=y_data, u_data_loc=u_data, v_data_loc=v_data)
